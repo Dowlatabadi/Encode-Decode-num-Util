c73dfbcb203be6ed1000ad5f56bb4ae9c01435cf
mehrdad8 <43187348+mehrdad8@users.noreply.github.com>
2020-03-20 22:12:47 +0330

    first

---
+++ CodeHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encode_Decode
+{
+    public static class CodeHelper
+    {
+        public static string Encode(long original_Num, bool Parity = false)
+        {
+
+        }
+        public static int Decode(string Encoded, bool Parity = false)
+        {
+
+        }
+    }
+}
